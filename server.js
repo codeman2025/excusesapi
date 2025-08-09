30eb37b8c3460b4f4f59e958e2c26528b4fcfb8b
Lucas <145366816+codeman2025@users.noreply.github.com>
2025-08-10 00:48:10 +0100

    Update server.js

--- server.js
+++ server.js
@@ -147,6 +147,11 @@ app.delete('/api/excuses/:id', authenticateJWT, (req, res) => {
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
+// Add this at the very end of your server.js, after all your routes and static middleware
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
